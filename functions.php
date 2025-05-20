d523a63451dbaa8b90c24ca3c215f62b27915c62
gherreramusica <132178396+gherreramusica@users.noreply.github.com>
2025-05-20 17:29:57 -0500

    updates

--- functions.php
+++ functions.php
@@ -100,23 +100,13 @@ function oromar_add_programa_relacionado_filter() {
 }
 add_action('rest_api_init', 'oromar_add_programa_relacionado_filter');
 
-function agregar_banner_a_programas() {
-  register_rest_field('programas', 'banner', [
+function exponer_banner_url_programa() {
+  register_rest_field('programas', 'banner_url', [
     'get_callback' => function($post_arr) {
-      $id = get_field('banner', $post_arr['id']);
-      return $id ? wp_get_attachment_url($id) : '';
+      return get_field('banner', $post_arr['id']) ?: '';
     },
     'schema' => null,
   ]);
 }
-add_action('rest_api_init', 'agregar_banner_a_programas');
+add_action('rest_api_init', 'exponer_banner_url_programa');
 
-function exponer_external_featured_image() {
-  register_rest_field('programas', 'external_image', [
-    'get_callback' => function($post_arr) {
-      return get_field('external_featured_image', $post_arr['id']);
-    },
-    'schema' => null,
-  ]);
-}
-add_action('rest_api_init', 'exponer_external_featured_image');
